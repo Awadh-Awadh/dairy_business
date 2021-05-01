02f25e0a35cc2377ae324f98bd3c581904cc04cd
Awadh Awadh <awadh.awadh@student.moringaschool.com>
2021-05-02 00:46:50 +0300

    add object that includes all the leap year months

--- index.js
+++ index.js
@@ -32,8 +32,36 @@
   incomeOverTime(45, 7);
 
 
-  let
+  let monthlyInfo = {
+    january: 31,
+    february: 29,
+    march: 31,
+    april: 30,
+    may: 31,
+    june: 30,
+    july: 31,
+    august: 31,
+    september: 30,
+    october: 31,
+    november:30,
+    december: 31,
+  };
+  let dailyIncome = 45 * total;
 
+  let monthly = [monthlyInfo.january*dailyIncome, monthlyInfo.february*dailyIncome, monthlyInfo.march*dailyIncome, monthlyInfo.april*dailyIncome, monthlyInfo.may*dailyIncome, monthlyInfo.june*dailyIncome, monthlyInfo.july*dailyIncome, monthlyInfo.august*dailyIncome, monthlyInfo.september*dailyIncome, monthlyInfo.october*dailyIncome, monthlyInfo.november*dailyIncome, monthlyInfo.december*dailyIncome];
+  function monthlyIcome(){
 
-
+  `Your income for january is${monthlyInfo[0]}
+  Your Income for february is ${monthlyInfo[1]}
+  Your income for march is ${monthlyInfo[2]}
+  Your income for april is ${monthlyInfo[3]}
+  Your income for May is ${monthlyInfo[4]}
+  your income for June is ${monthlyInfo[5]}
+  Your income for July is ${monthlyInfo[6]}
+  Your income for August is ${monthlyInfo[7]}
+  Your income for September is ${monthlyInfo[8]}
+  Your income for october is ${monthlyInfo[9]}
+  Your income for November is ${monthlyInfo[10]}
+  Your income for december is ${monthlyInfo[11]}` ;
+}
 
